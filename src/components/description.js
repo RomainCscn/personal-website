f1c941932cdd5fd362359e38c5af0d4570fe83f0
Romain Cascino <contact@romaincascino.com>
2019-08-30 17:49:41 +0200

    Update description to add social network links

--- src/components/description.js
+++ src/components/description.js
@@ -3,6 +3,9 @@ import { Link } from 'gatsby';
 import Image from 'gatsby-image';
 import CV from './cv';
 
+import github from '../../content/assets/images/logo/github.svg';
+import linkedin from '../../content/assets/images/logo/linkedin.svg';
+
 const Description = props => (
   <div className='flex flex-col mb-16 px-3 py-6 bg-white rounded-lg shadow-custom sm:p-6 sm:flex-row sm:items-start md:items-center'>
     <Image
@@ -35,8 +38,25 @@ const Description = props => (
         parcours développement web
       </a>{' '}
       de la plateforme OpenClassrooms.
-      <div className='flex justify-center sm:justify-start'>
-        <CV className='mt-6' />
+      <div className='flex justify-center items-center mt-6 sm:justify-start'>
+        <CV className='mr-2' />
+        <a href='https://github.com/romaincscn'>
+          <img className='w-8 mx-2' src={github}></img>
+        </a>
+        <a href='https://www.linkedin.com/in/romaincascino/'>
+          <img className='w-8 mx-2' src={linkedin}></img>
+        </a>
+        <a href='mailto:contact@romaincascino.com'>
+          <svg
+            className='w-8 mx-2'
+            xmlns='http://www.w3.org/2000/svg'
+            viewBox='0 0 24 24'>
+            <g>
+              <path fill='none' d='M0 0h24v24H0z' />
+              <path fill='#38a169' d='M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z' />
+            </g>
+          </svg>
+        </a>
       </div>
     </div>
   </div>
