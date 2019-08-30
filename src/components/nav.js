9120448e9c233914a2c1ec8718c283d12418b9dd
Romain Cascino <contact@romaincascino.com>
2019-08-30 15:55:09 +0200

    Improve mobile menu looks

--- src/components/nav.js
+++ src/components/nav.js
@@ -5,7 +5,7 @@ const NavLink = props => {
   return (
     <Link
       activeClassName='active-nav'
-      className={`${props.className} font-semibold text-gray-600 mr-8`}
+      className={`${props.className} font-semibold text-gray-600 my-2 sm:my-0 sm:mr-8`}
       to={props.link}>
       {props.title}
     </Link>
@@ -21,14 +21,14 @@ const Nav = () => {
         title='Romain Cascino'
         link='/'></NavLink>
       <div
-        className='block sm:hidden float-right cursor-pointer'
+        className='block sm:hidden float-right cursor-pointer mt-1'
         onClick={() => setShowMobileMenu(!showMobileMenu)}>
         Menu
       </div>
       <div
         className={`${
           showMobileMenu ? 'flex' : 'hidden'
-        } flex-col items-center text-lg sm:block sm:float-right`}>
+        } flex-col items-center text-2xl mt-6 sm:block sm:float-right sm:text-lg sm:mt-0`}>
         <NavLink title='À propos' link='/about'></NavLink>
         <NavLink title='Projets' link='/projects'></NavLink>
         <NavLink title='Blog' link='/blog'></NavLink>
